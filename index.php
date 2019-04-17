1b189c738acec40f76a3457ee40273e0f257119b
Kaephas <kkain@mail.greenriver.edu>
2019-04-17 17:10:51 -0400

    all the files

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
  * Description: loads error reporting, composer, fat free, setting default route to views/home.html
  */
 
+session_start();
+
 //Turn on error reporting
 ini_set('display_errors' ,1);
 error_reporting(E_ALL);
@@ -46,6 +48,68 @@ $f3->route('GET /lunch', FUNCTION()
     // display lunch view
     $view = new Template();
     echo $view-> render('views/lunch.html');
+});
+
+$f3->route('GET /lunch/brunch/buffet', FUNCTION()
+{
+    // display lunch view
+    $view = new Template();
+    echo $view-> render('views/brunch.html');
+});
+
+// define a route with a parameter
+$f3->route('GET /@item', function($f3, $params)
+{
+    $item = $params['item'];
+
+
+
+    switch($item) {
+        case 'spaghetti':
+            echo "<h3>I like $item with meatballs.</h3>";
+            break;
+
+        case 'pizza':
+            echo "<h3>Pepperoni or veggie?</h3>";
+            break;
+
+        case 'tacos':
+            echo "<h3>We don't have $item.</h3>";
+            break;
+
+        case 'bagel':
+            $f3->reroute("/breakfast/continental");
+
+        default:
+            $f3->error(404);
+    }
+});
+
+$f3->route('GET /@first/@last', function($f3, $names) {
+
+    echo ucfirst($names['first']) . " " . ucfirst($names['last']);
+});
+
+$f3->route('GET /order', function() {
+    // new form view
+    $view = new Template();
+    echo $view->render("views/form1.html");
+});
+
+$f3->route('POST /meal', function() {
+//    print_r($_POST);
+    $_SESSION['food'] = $_POST['food'];
+
+    $view = new Template();
+    echo $view->render("views/form2.html");
+});
+
+$f3->route('POST /summary', function() {
+//    print_r($_POST);
+    $_SESSION['meal'] = $_POST['meal'];
+
+    $view = new Template();
+    echo $view->render("views/summary.html");
 });
 
 //run Fat-free
